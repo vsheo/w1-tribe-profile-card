1f8c018080bd227f6200511ef2f28e1a574fe765
vsheo <107566336+vsheo@users.noreply.github.com>
2024-09-03 12:17:37 +0200

    header
    columns en rows toegevoegd in de header om een layout te maken.
    
    profile picture draait om als je erop hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+// const expandButton = document.querySelector('header button')
+// expandButton.addEventListener('click', expand)
 
-function expand () {
-  document.body.classList.toggle('expand')
+// function expand () {
+//   document.body.classList.toggle('expand')
+// }
+
+// flip profile image
+// source: https://codepen.io/mondal10/pen/WNNEvjV
+const flipImage = document.querySelector('header imageContainer')
+
+flipImage.addEventListener('click', flipperClick)
+
+function flipper () {
+  card.classList.toggle('is-flipped');
 }
