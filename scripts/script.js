f18c1c72764ca0db367f003dcf6a482e84d6b85c
vsheo <vireshsheoratan@gmail.com>
2024-09-05 00:50:25 +0200

    list + hover
    list gemaakt met dl, dt en dd
    grid gebruikt om alles op de juiste plaats te krijgen.
    en een pop over met hover toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,3 @@
 // function expand () {
 //   document.body.classList.toggle('expand')
 // }
-
-// flip profile image
-// source: https://codepen.io/mondal10/pen/WNNEvjV
-const flipImage = document.querySelector('header imageContainer')
-
-flipImage.addEventListener('click', flipperClick)
-
-function flipper () {
-  card.classList.toggle('is-flipped');
-}
