261679e97447bfa5614f62fcccb4e5849eca4e71
vsheo <vireshsheoratan@gmail.com>
2024-09-05 16:40:11 +0200

    list hover fixed
    flip werkt niet goed
    profile picture is niet goed geplaatst

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,11 @@
 // function expand () {
 //   document.body.classList.toggle('expand')
 // }
+
+var card = document.querySelector('.card')
+card.addEventListener('click', flip)
+
+function flip () {
+  card.classList.toggle('is-flipped')
+};
+
